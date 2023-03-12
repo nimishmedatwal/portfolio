08918d18484b5edaab1bc15ce157e62a57b2925f
nimishmedatwal <medatwalnimish@gmail.com>
2023-03-13 00:34:25 +0530

    edited navbar.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { styles } from '../styles';
+import { navlinks } from '../constants';
+import { logo, menu, close } from '../assets'
 const Navbar = () => {
   return (
     <div>Navbar</div>
